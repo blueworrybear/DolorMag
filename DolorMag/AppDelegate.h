6e616fe3a53ad499d7fcdb9bc2540e00bf8ccf40
bear <blueworrybear@gmail.com>
2012-04-04 20:43:48 +0800

    Initial Commit

---
+++ DolorMag/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  DolorMag
+//
+//  Created by bear on 12/4/4.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ DolorMag/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DolorMag
+//
+//  Created by bear on 12/4/4.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
